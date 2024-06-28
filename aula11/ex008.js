9978a8e41642d4cd45dfa49684764bbe264ca2aa
Bruno Rodrigues Marques <brunomarque2002@gmail.com>
2024-06-27 21:54:33 -0300

    Aula de condições

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var lim = 50
+var vel = 25
+
+console.log (`Sua velocidade está em ${vel}`)
+if (vel > lim) { // Condição SIMPLES
+    console.log (`Sua velocidade ultrapassou o limite permitido na via! MULTADO`)
+
+} 
+
+console.log (`Continue no mesmo ritmo e utilize o cinto de segurança`)
